57b89cbc1eeaac20f590529404b2097cfed490e4
Debmalya Pramanik <DebmalyaPramanik.005@gmail.com>
2021-09-30 23:03:10 +0530

    setup basic files for python wrapper
    * included setup options
    * included change logs
    * updated documentation, flake8 convention, version file

---
+++ VisualCrossing/__init__.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+
+"""Visual Crossing Weather Data Fetching using Python"""
+
+from os.path import join
+from pathlib import Path
+
+# setting homepath, as provided in setup.py
+# TODO detailed documentation for API/Configuration File
+__homepath__ = join(Path.home(), ".visualCrossing")
+
+# init-time option registrations
---
+++ setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+"""A setuptools based model for setting up python wrapper for VisualCrossing API"""
+
+from os import makedirs
+from os.path import join
+from pathlib import Path
+from setuptools import setup
+from setuptools import find_packages
+
+PKG = "VisualCrossing" # package-name
+
+# Version File Implementation: https://stackoverflow.com/a/7071358
+VERSIONFILE = join("static", "VERSION")
+try:
+    VERSION = open(VERSIONFILE, "rt").read() # always read as str()
+except FileNotFoundError as err:
+    raise RuntimeError("unable to run setuptools")
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# create homepath for logging
+# also, homepath can be used for finding API Key/Configuration File(s)
+HOMEPATH = join(Path.home(), ".visualCrossing")
+
+if not isdir(HOMEPATH):
+    # create directory for first time installation
+    # else, check additional configuration for the model
+    # TODO setup additional details file for module configuration
+    makedirs(HOMEPATH)
+
+setup(
+        name         = PKG,
+        version      = VERSION,
+        author       = "Debmalya Pramanik",
+        author_email = "dPramanik.official@gmail.com",
+
+        description                   = "A python wrapper for fetching weather data from VisualCrossing",
+        long_description              = long_description,
+        long_description_content_type = "text/markdown",
+
+        url         = "https://github.com/ZenithClown/VisualCrossing",
+        packages    = find_packages(),
+        classifiers = [
+            "Development Status :: 1 - Planning",
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License"
+        ],
+        python_requires  = ">=3.7",
+        install_requires = [],
+        keywords = "weather-api, weather-forecast, visual-crossing, api-rest, wrapper"
+    )
